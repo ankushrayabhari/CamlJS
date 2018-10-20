3d1542be41ed6428338ab980d0e9ac5aeeca44d1
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-10-20 16:40:47 -0400

    Added initial definition of AST

---
+++ parser.mli
@@ -0,0 +1,29 @@
+type constant =
+  | Int of int
+
+type infix_op =
+  | Plus | Minus | Divide | Times | GreaterThan | LessThan | GreaterThanOrEqual
+  | LessThanOrEqual | Equal | NotEqual
+
+type prefix_symbol =
+  | Negation
+
+type value_name =
+  | LowercaseIdent of string
+
+type pattern =
+  | ValueName of value_name
+
+type let_binding =
+  | VarAssignment of pattern * expr
+  | FunctionAssignment of value_name * bool * pattern list * expr
+and expr =
+  | Constant of constant
+  | PrefixOp of prefix_symbol * expr
+  | InfixOp of expr * infix_op * expr
+  | Ternary of expr * expr * expr option
+  | Function of pattern list * expr
+  | Sequential of expr * expr
+  | ValueName of value_name
+
+val parse : Tokenizer.token list -> expr
