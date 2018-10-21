a4ef5120fdc10aafc32d5f471e20086fcf4b16bd
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-10-21 18:11:17 -0400

    Fixed the parser
    - Fixed bug where LowercaseIdent expr weren't being parsed properly
    - Fixed bug where prev array in CYK wasn't being set properly
    - Formatted parser.ml to not exceed 80 lines

--- parser.ml
+++ parser.ml
@@ -55,9 +55,11 @@ let token_to_varid = Tokenizer.(function
   )
 
 let rules = [
-  [(15, 26); (12, 25); (25, 27); (17, 28); (20, 32); (25, 35); (22, 36); (22, 37); (25, 25)]; (* 25 *)
+  [(15, 26); (12, 25); (25, 27); (17, 28); (20, 32); (25, 35); (22, 36);
+   (22, 37); (25, 25)]; (* 25 *)
   [(25, 16)]; (* 26 *)
-  [(2, 25); (3, 25); (4, 25); (5, 25); (6, 25); (7, 25); (8, 25); (9, 25); (10, 25); (11, 25)]; (* 27 *)
+  [(2, 25); (3, 25); (4, 25); (5, 25); (6, 25); (7, 25); (8, 25); (9, 25);
+   (10, 25); (11, 25)]; (* 27 *)
   [(25, 29)]; (* 28 *)
   [(18, 25); (18, 30)]; (* 29 *)
   [(25, 31)]; (* 30 *)
@@ -73,22 +75,89 @@ let rules = [
   [(24, 25)]; (* 40 *)
 ]
 
-type variable_tree = Cons of int * int * int * variable_tree * variable_tree | Nil
+type variable_tree =
+| Cons of int * int * int * variable_tree * variable_tree
+| Nil
+type parse_tree =
+  | Const of constant
+  | Infix of infix_op
+  | Prefix of prefix_symbol
+  | Value of value_name
+  | Binding of let_binding
+  | Expr of expr
 
-let variable_tree_to_constant tok_arr = function
-  | Nil -> failwith "don't call"
-  | Cons (v, s, e, l, r) -> match v with
-    | 1 -> begin match tok_arr.(s) with
-      | Tokenizer.Int s -> Int s
-      | _ -> failwith "failed to parse constant"
-    end
-    | _ -> failwith "argument is not a constant"
+let production_of_root_of_variable_tree = function
+  | Cons (_, _, _, Cons (l, _, _, _, _), Cons (r, _, _, _, _)) -> Some (l, r)
+  | _ -> None
+
+let parse_infix_op tok_arr = function
+  | Nil -> failwith "should not be called on nil"
+  | Cons (_, s, _, _, _) -> begin match tok_arr.(s) with
+    | Tokenizer.Plus -> Plus
+    | _ -> failwith "infix operation not supported"
+  end
+
+let parse_token_expr tok_arr = function
+  | Nil -> failwith "should not be called on nil"
+  | Cons (_, s, _, _, _) -> begin match tok_arr.(s) with
+    | Tokenizer.Int v -> Constant (Int v)
+    | Tokenizer.LowercaseIdent v -> VarName (LowercaseIdent v)
+    | _ -> failwith "unsupported token expr value"
+  end
+
+let rec parse_expr tok_arr = function
+  | Nil -> failwith "should not be called on nil"
+  | Cons (v, s, e, l, r) as t ->
+      match production_of_root_of_variable_tree t with
+        | None -> parse_token_expr tok_arr t
+        | Some (15, 26) -> begin match r with
+            | Cons (_, _, _, lr, _) -> parse_expr tok_arr lr
+            | _ -> failwith "not a parenthesized expr"
+            end
+        | Some (12, 25) -> failwith "prefix op not implemented"
+        | Some (25, 27) ->
+            let left_operand = parse_expr tok_arr l in
+            begin match r with
+              | Cons (_, _, _, lr, rr) ->
+                let operator = parse_infix_op tok_arr lr in
+                let right_operand = parse_expr tok_arr rr in
+                InfixOp (left_operand, operator, right_operand)
+              | _ -> failwith "not an infix expr"
+            end
+        | Some (17, 28) -> failwith "if expr not implemented"
+        | Some (20, 32) -> failwith "fun expr not implemented"
+        | Some (25, 35) -> failwith "semicolon expr not implemented"
+        | Some (22, 36) -> failwith "let rec not implemented"
+        | Some (22, 37) -> begin match r with
+            | Cons (_, _, _,
+                Cons (_, _, _,
+                  Cons (_, ident_token_index, _, _, _),
+                  Cons (_, _, _, _, assignment_expr_var_tree)
+                ),
+                Cons (_, _, _, _, in_expr_var_tree)
+              ) -> begin  match tok_arr.(ident_token_index) with
+                | Tokenizer.LowercaseIdent s ->
+                    LetBinding (
+                      VarAssignment (
+                        ValueName (LowercaseIdent s),
+                        parse_expr tok_arr assignment_expr_var_tree
+                      ),
+                      parse_expr tok_arr in_expr_var_tree
+                    )
+                | _ -> failwith "invalid let tree"
+                end
+            | _ -> failwith "not a valid let binding"
+          end
+        | Some (25, 25) -> failwith "function call not implemented"
+        | _ -> failwith "invalid production rule"
 
 
 let parse tok_arr =
   let n = Array.length tok_arr in
-  let dp = Array.init n (fun _ -> Array.init n (fun _ -> (Array.make 42 false))) in
-  let prev = Array.init n (fun _ -> Array.init n (fun _ -> (Array.make 42 (-1, -1, -1)))) in
+  let dp = Array.init n
+    (fun _ -> Array.init n (fun _ -> (Array.make 42 false))) in
+  let prev = Array.init n
+    (fun _ -> Array.init n (fun _ -> (Array.make 42 (-1, -1, -1)))) in
   for s = 0 to n - 1 do
     List.iter (fun v ->
         dp.(s).(s).(v) <- true;
@@ -102,31 +171,27 @@ let parse tok_arr =
         List.iteri (fun a el ->
             let a = a + 25 in
             List.iter (fun (b, c) ->
-                if not dp.(s).(s + l).(a) && dp.(s).(m).(b) && dp.(m + 1).(s + l).(c)
-                then dp.(s).(s + l).(a) <- true; prev.(s).(s+l).(a) <- (b, m, c)
+                if not dp.(s).(s + l).(a) &&
+                   dp.(s).(m).(b) &&
+                   dp.(m + 1).(s + l).(c)
+                then begin
+                  dp.(s).(s + l).(a) <- true;
+                  prev.(s).(s+l).(a) <- (b, m, c)
+                end
               ) el
           ) rules
       done
     done
   done;
 
-  for i = 0 to n - 1 do
-    print_endline (string_of_int i ^ ": ");
-    for j = 0 to n - 1 do
-      print_string ("\t" ^ string_of_int j ^ ": ");
-      for k = 1 to 41 do
-        if dp.(i).(j).(k) then print_string (string_of_int k ^ "; ")
-      done;
-      print_endline ""
-    done
-  done;
-  if not dp.(n - 1).(n - 1).(25) then failwith "Invalid program."
+  if not dp.(0).(n - 1).(25) then failwith "Invalid program."
   else
     let rec generate_var_tree s e v =
-      if (s = -1 || e = -1) then Nil else
+      if (s = e) then Cons (v, s, e, Nil, Nil) else
       let (left_var, middle_index, right_var) = prev.(s).(e).(v) in
       let left_parse_tree = generate_var_tree s middle_index left_var in
       let right_parse_tree = generate_var_tree (middle_index + 1) e right_var in
       Cons (v, s, e, left_parse_tree, right_parse_tree)
     in
-    generate_var_tree 0 (n - 1) 25
+    let expr_var_tree = generate_var_tree 0 (n - 1) 25 in
+    parse_expr tok_arr expr_var_tree
