b1ce3d9be8f6a63d5a9df0d5f3d909575cf9180b
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-10-22 17:23:42 -0400

    Finished anonymous functions and sequential op rendering

--- renderer.ml
+++ renderer.ml
@@ -27,11 +27,13 @@ let rec render_let_binding = function
       let assignment_expr = render_expr expr in
       "let " ^ var_name ^ " = " ^ assignment_expr ^  ";"
   | FunctionAssignment (name_value, _, arg_list, body_expr) ->
-      let function_body = render_expr body_expr in
-      let arguments = List.map render_pattern arg_list |> String.concat "," in
       let function_name = render_value_name name_value in
-      "let " ^ function_name ^ " = (" ^ arguments ^ ") =>" ^
-        function_body ^  ";"
+      "let " ^ function_name ^ " = " ^ render_fun arg_list body_expr ^  ";"
+
+and render_fun arg_list body_expr =
+  let function_body = render_expr body_expr in
+  let arguments = List.map render_pattern arg_list |> String.concat "," in
+  "((" ^ arguments ^ ") =>" ^ function_body ^ ")"
 
 and render_prefix_op = function
   | Negation -> "-"
@@ -48,8 +50,9 @@ and render_expr = function
       let rendered_op = render_infix_op op in
       "(() => (" ^ rendered_l ^ ")" ^ rendered_op ^ "(" ^ rendered_r ^ "))()"
   | Ternary (condition, true_expr, false_expr) -> failwith "ternary not implemented"
-  | Function (arg_list, body)-> failwith "functions not implemented"
-  | Sequential (expr_1, expr_2) -> failwith "sequential ops not implemented"
+  | Function (arg_list, body_expr) -> render_fun arg_list body_expr
+  | Sequential (expr_1, expr_2) ->
+      "(() => {" ^ render_expr expr_1 ^ "; return " ^ render_expr expr_2 ^ "})()"
   | LetBinding (assign, expr) ->
       let rendered_assign = render_let_binding assign in
       let rendered_in_expr = render_expr expr in
