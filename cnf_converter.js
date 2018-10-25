2721f155a17750a3dbe40b9e8db25d030e82d896
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-10-25 12:12:52 -0400

    Updated formatting in CNF converter

--- cnf_converter.js
+++ cnf_converter.js
@@ -22,7 +22,8 @@ let get_variable_id = (name) => {
 }
 
 let is_unit_production = (production) => {
-  return production.length == 1 && get_variable_id(production[0]) >= num_tokens;
+  return production.length == 1 &&
+         get_variable_id(production[0]) >= num_tokens;
 }
 
 let get_productions = (var_id) => {
@@ -39,14 +40,12 @@ let set_productions = (var_id, new_prod) => {
   productions[variables[var_id - num_tokens]] = no_duplicates_productions;
 }
 
-// Step 1 - Get rid of all unit productions
+// Step 2 - Get rid of all unit productions
 let remove_unit_productions = (var_id) => {
   let to_add_productions = [];
   let to_remove = new Set();
-  let productions = get_productions(var_id);
 
-
-  for (let production of productions) {
+  for (let production of get_productions(var_id)) {
     if (is_unit_production(production)) {
       let sub_variable = get_variable_id(production[0]);
       to_remove.add(production[0]);
@@ -65,5 +64,5 @@ let remove_unit_productions = (var_id) => {
   );
 }
 
-remove_unit_productions(start_variable);
 console.log(productions);
+remove_unit_productions(start_variable);
