8ad5ba41fded709f45026fe1a638bb3a0bec3f3f
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-10-25 14:08:07 -0400

    Added rules array printer in converter

--- cnf_converter.js
+++ cnf_converter.js
@@ -18,6 +18,7 @@ for (let i = 0; i < num_variables; i++) {
   variable_to_id_map.set(variables[i], i + num_tokens);
 }
 
+// Helper functions for accessing production information
 let get_variable_id = (name) => {
   return variable_to_id_map.get(name);
 }
@@ -56,12 +57,13 @@ let is_large_production = (production) => {
   return production.length > 2;
 }
 
+let is_token_production = (production) => {
+  return production.length == 1 && get_variable_id(production[0]) < num_tokens;
+}
+
 // Step 1 - Get rid of all productions larger than 3
 let remove_large_productions = () => {
-  console.log(start_variable);
-  console.log(last_variable);
   for (let var_id = start_variable; var_id <= last_variable; var_id++) {
-
     for (let production of get_productions(var_id)) {
       while (is_large_production(production)) {
         let last_el = production.pop();
@@ -97,7 +99,47 @@ let remove_unit_productions = (var_id) => {
   );
 }
 
-
 remove_large_productions();
 remove_unit_productions(start_variable);
-console.log(productions);
+
+// Step 3 - Write the token_to_varid function
+let token_to_varid_fn = () => {
+  let tokenToVarId = new Map();
+  for (let var_id = 0; var_id < num_tokens; var_id++) {
+    tokenToVarId.set(tokens[var_id].name, [var_id]);
+  }
+
+  for (let var_id = start_variable; var_id <= last_variable; var_id++) {
+    for (let production of get_productions(var_id)) {
+      if (is_token_production(production)) {
+        let val = tokenToVarId.get(production[0]);
+        val.push(var_id);
+        tokenToVarId.set(production[0], val);
+      }
+    }
+  }
+
+  let match_cases = "";
+  for (let token of tokens) {
+    match_cases += `  | ${token.name} ${token.parameter ? "_ " : ""}-> ` +
+                   `[${tokenToVarId.get(token.name).join(";")}]\n`;
+  }
+
+  return `let token_to_varid = Tokenizer.(function\n${match_cases})`
+}
+
+// Step 4 - Write out the rules
+let rules_lst = () => {
+  let rules_lst = "";
+  for (let var_id = start_variable; var_id < num_variables; var_id++) {
+    let string_productions =
+      get_productions(var_id)
+      .filter(production => !is_token_production(production))
+      .map((production) => `(${production.map(get_variable_id).join(",")})`)
+      .join(";");
+    rules_lst += `\n  [${string_productions}];`;
+  }
+  return `let rules = [${rules_lst}\n]`;
+}
+
+console.log(rules_lst());
