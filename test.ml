01a0d875878cbff8b18d7b9687aaf4a2e8a00a9a
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-10-27 19:21:22 -0400

    Fixed tests

--- test.ml
+++ test.ml
@@ -1,63 +1,128 @@
 open OUnit2
 
-let tokenizer_tests = Tokenizer.[
-  "basic_test" >:: (fun _ ->
-    let tokenized_program = tokenize "let x = 1 in x + 100" in
-    let expected_tokens = [
-      Let;
-      LowercaseIdent "x";
-      Equal;
-      Int 1;
-      In;
-      LowercaseIdent "x";
-      Plus;
-      Int 100;
-    ] in
-    assert_equal expected_tokens tokenized_program
-  );
+let make_tokenizer_test name program expected_value =
+  name >:: (fun _ ->
+    let tokenized_program = Tokenizer.tokenize program in
+    assert_equal expected_value tokenized_program
+  )
 
-  "if statement test" >:: (fun _ ->
-    let tokenized_program = tokenize "if 0 = 0 then 0 else 1" in
-    let expected_tokens = [
-      If;
-      Int 0;
-      Equal;
-      Int 0;
-      Then;
-      Int 0;
-      Else;
-      Int 1;
-    ] in
-    assert_equal expected_tokens tokenized_program
-  );
+let tokenizer_tests = Tokenizer.[
+  make_tokenizer_test
+    "let assign test"
+    "let x = 1 in x + 100"
+    [Let; LowercaseIdent "x"; Equal; Int 1; In; LowercaseIdent "x"; Plus;
+     Int 100;];
+
+  make_tokenizer_test
+    "let rec function test with paren expression"
+    "let rec sum x = x + sum (x - 1)"
+    [Let; Rec; LowercaseIdent "sum"; LowercaseIdent "x"; Equal;
+     LowercaseIdent "x"; Plus; LowercaseIdent "sum"; LParen;
+     LowercaseIdent "x"; Minus; Int 1; RParen;];
+
+  make_tokenizer_test
+    "greater than comparison expression"
+    "1 > 2"
+    [Int 1; GreaterThan; Int 2];
+
+  make_tokenizer_test
+    "less than comparison expression"
+    "1 < 2"
+    [Int 1; LessThan; Int 2];
+
+  make_tokenizer_test
+    "greater than or equal comparison expression"
+    "1 >= 2"
+    [Int 1; GreaterThanOrEqual; Int 2];
+
+  make_tokenizer_test
+    "less than or equal comparison expression"
+    "1 <= 2"
+    [Int 1; LessThanOrEqual; Int 2];
+
+  make_tokenizer_test
+    "is equal comparison expression"
+    "1 = 2"
+    [Int 1; Equal; Int 2];
+
+  make_tokenizer_test
+    "not equal comparison expression"
+    "1 <> 2"
+    [Int 1; NotEqual; Int 2];
+
+  make_tokenizer_test
+    "if then no else expression"
+    "if 1 = 2 then 0"
+    [If; Int 1; Equal; Int 2; Then; Int 0];
+
+  make_tokenizer_test
+    "if then with else expression"
+    "if 1 = 2 then 0 else 5"
+    [If; Int 1; Equal; Int 2; Then; Int 0; Else; Int 5];
+
+  make_tokenizer_test
+    "if statement test"
+    "if 0 <> 0 then 0 else 1"
+    [If; Int 0; NotEqual; Int 0; Then; Int 0; Else; Int 1; ];
+
+  make_tokenizer_test
+    "anonymous function expression"
+    "fun a b -> a + b"
+    [Fun; LowercaseIdent "a"; LowercaseIdent "b"; FunctionArrow;
+     LowercaseIdent "a"; Plus; LowercaseIdent "b"];
+
+  make_tokenizer_test
+    "semicolon expression"
+    "1;2"
+    [Int 1; SemiColon; Int 2];
+
+  make_tokenizer_test
+    "times infix expression"
+    "1 * 2"
+    [Int 1; Times; Int 2];
+
+  make_tokenizer_test
+    "divide infix expression"
+    "1 / 2"
+    [Int 1; Divide; Int 2];
+
+  make_tokenizer_test
+    "negation expression"
+    "~-1"
+    [Negation; Int 1;];
 ]
 
-let parser_tests = Parser.[
-  "basic_test" >:: (fun _ ->
-    let tokenized_program =
-      Tokenizer.tokenize "let x = 1 in x + 100" |> Array.of_list in
-    let parse_tree = parse tokenized_program in
-    let expected_tree =
-      LetBinding (
-        VarAssignment (
-          ValueName (LowercaseIdent "x"),
-          Constant (Int 1)
-        ),
-        InfixOp (
-          VarName (LowercaseIdent "x"),
-          Plus,
-          Constant (Int 100)
-        )
-      ) in
-    assert_equal expected_tree parse_tree
-  );
+let make_ast_converter_test name program expected_tree =
+  name >:: (fun _ ->
+    let ast =
+      Tokenizer.tokenize program
+      |> Array.of_list
+      |> Parser.parse
+      |> Ast.convert
+    in
+    assert_equal expected_tree ast
+  )
 
-  "order of ops in infix expression" >:: (fun _ ->
-    let tokenized_program =
-      Tokenizer.tokenize "let x = 1 in x - 100 - 200" |> Array.of_list in
-    let parse_tree = parse tokenized_program in
-    let expected_tree =
-      LetBinding (
+let ast_converter_tests = Ast.[
+  make_ast_converter_test
+    "basic_test"
+    "let x = 1 in x + 100"
+    (LetBinding (
+      VarAssignment (
+        ValueName (LowercaseIdent "x"),
+        Constant (Int 1)
+      ),
+      InfixOp (
+        VarName (LowercaseIdent "x"),
+        Plus,
+        Constant (Int 100)
+      )
+    ));
+
+  make_ast_converter_test
+    "order of ops in infix expression"
+    "let x = 1 in x - 100 - 200"
+    (LetBinding (
         VarAssignment (
           ValueName (LowercaseIdent "x"),
           Constant (Int 1)
@@ -71,18 +136,12 @@ let parser_tests = Parser.[
           Minus,
           Constant (Int 200)
         )
-      ) in
-    assert_equal expected_tree parse_tree
-  );
+      ));
 
-  "order of ops in infix expression parentheiszed" >:: (fun _ ->
-    let tokenized_program =
-      Tokenizer.tokenize "let x = 1 in (x - 100) - (200 - x)"
-      |> Array.of_list
-    in
-    let parse_tree = parse tokenized_program in
-    let expected_tree =
-      LetBinding (
+  make_ast_converter_test
+    "order of ops in infix expression parentheiszed"
+    "let x = 1 in (x - 100) - (200 - x)"
+    (LetBinding (
         VarAssignment (
           ValueName (LowercaseIdent "x"),
           Constant (Int 1)
@@ -99,102 +158,151 @@ let parser_tests = Parser.[
             Minus,
             VarName (LowercaseIdent "x")
           ))
-        )
-      ) in
-    assert_equal expected_tree parse_tree
-  );
+        )));
 
-  "order of ops in infix expression parentheiszed nested" >:: (fun _ ->
-    let tokenized_program =
-      Tokenizer.tokenize "let x = 1 in (x - 100 - 300) - (200 - x)"
-      |> Array.of_list
-    in
-    let parse_tree = parse tokenized_program in
-    let expected_tree =
-      LetBinding (
-        VarAssignment (
-          ValueName (LowercaseIdent "x"),
-          Constant (Int 1)
-        ),
-        InfixOp (
-          ParenExpr (
-            InfixOp (
-              InfixOp (
-                VarName (LowercaseIdent "x"),
-                Minus,
-                Constant (Int 100)
-              ),
-              Minus,
-              Constant (Int 300)
-            )
+  make_ast_converter_test
+    "order of ops in infix expression parentheiszed nested"
+    "let x = 1 in (x - 100 - 300) - (200 - x)"
+    (LetBinding (
+          VarAssignment (
+            ValueName (LowercaseIdent "x"),
+            Constant (Int 1)
           ),
-          Minus,
-          ParenExpr (InfixOp (
-            Constant (Int 200),
-            Minus,
-            VarName (LowercaseIdent "x")
-          ))
-        )
-      ) in
-    assert_equal expected_tree parse_tree
-  );
-
-  "function call" >:: (fun _ ->
-    let tokenized_program =
-      Tokenizer.tokenize "let rec x a b c = a * b * c in x 1 2 3"
-      |> Array.of_list
-    in
-    let parse_tree = parse tokenized_program in
-    let expected_tree =
-      LetBinding (
-        FunctionAssignment (
-          LowercaseIdent "x",
-          true,
-          [
-            ValueName (LowercaseIdent "a");
-            ValueName (LowercaseIdent "b");
-            ValueName (LowercaseIdent "c");
-          ],
           InfixOp (
-            InfixOp (
-              VarName (LowercaseIdent "a"),
-              Times,
-              VarName (LowercaseIdent "b")
+            ParenExpr (
+              InfixOp (
+                InfixOp (
+                  VarName (LowercaseIdent "x"),
+                  Minus,
+                  Constant (Int 100)
+                ),
+                Minus,
+                Constant (Int 300)
+              )
             ),
-            Times,
-            VarName (LowercaseIdent "c")
+            Minus,
+            ParenExpr (InfixOp (
+              Constant (Int 200),
+              Minus,
+              VarName (LowercaseIdent "x")
+            ))
           )
-        ),
+        ));
+
+  make_ast_converter_test
+    "function call"
+    "let rec x a b c = a * b * c in x 1 2 3"
+    (LetBinding (
+      FunctionAssignment (
+        LowercaseIdent "x",
+        true,
+        [
+          ValueName (LowercaseIdent "a");
+          ValueName (LowercaseIdent "b");
+          ValueName (LowercaseIdent "c");
+        ],
+        InfixOp (
+          InfixOp (
+            VarName (LowercaseIdent "a"),
+            Times,
+            VarName (LowercaseIdent "b")
+          ),
+          Times,
+          VarName (LowercaseIdent "c")
+        )
+      ),
+      FunctionCall (
         FunctionCall (
           FunctionCall (
-            FunctionCall (
-              VarName (LowercaseIdent "x"),
-              Constant (Int 1)
-            ),
-            Constant (Int 2)
+            VarName (LowercaseIdent "x"),
+            Constant (Int 1)
           ),
-          Constant (Int 3)
-        )
-      ) in
-    assert_equal expected_tree parse_tree
-  );
-]
+          Constant (Int 2)
+        ),
+        Constant (Int 3)
+      )
+    ));
 
-let renderer_tests = Renderer.[
-  "basic_test" >:: (fun _ ->
-      Tokenizer.tokenize "let x = 1 in x + 100"
-      |> Array.of_list
-      |> Parser.parse
-      |> render
-      |> assert_equal
-          "(() => {let x = (() => 1)();return (() => (x)+((() => 100)()))()})()"
-  );
+  make_ast_converter_test
+    "greater than comparison expression"
+    "1 > 2"
+    (InfixOp (Constant (Int 1), GreaterThan, (Constant (Int 2))));
+
+  make_ast_converter_test
+    "less than comparison expression"
+    "1 < 2"
+    (InfixOp (Constant (Int 1), LessThan, (Constant (Int 2))));
+
+  make_ast_converter_test
+    "less than or equal comparison expression"
+    "1 <= 2"
+    (InfixOp (Constant (Int 1), LessThanOrEqual, (Constant (Int 2))));
+
+  make_ast_converter_test
+    "greater than or equal comparison expression"
+    "1 >= 2"
+    (InfixOp (Constant (Int 1), GreaterThanOrEqual, (Constant (Int 2))));
+
+  make_ast_converter_test
+    "is equal comparison expression"
+    "1 = 2"
+    (InfixOp (Constant (Int 1), Equal, (Constant (Int 2))));
+
+  make_ast_converter_test
+    "not equal comparison expression"
+    "1 <> 2"
+    (InfixOp (Constant (Int 1), NotEqual, (Constant (Int 2))));
+
+  make_ast_converter_test
+    "if then no else expression"
+    "if 1 = 2 then 0"
+    (Ternary (
+      InfixOp (Constant (Int 1), Equal, (Constant (Int 2))),
+      Constant (Int 0),
+      None));
+
+  make_ast_converter_test
+    "if then with else expression"
+    "if 1 = 2 then 0 else 5"
+    (Ternary (
+      InfixOp (Constant (Int 1), Equal, (Constant (Int 2))),
+      Constant (Int 0),
+      Some (Constant (Int 5))));
+
+  make_ast_converter_test
+    "anonymous function expression"
+    "fun a b -> a + b"
+    (Function (
+      [ValueName (LowercaseIdent "a"); ValueName (LowercaseIdent "b")],
+      InfixOp (
+        VarName (LowercaseIdent "a"), Plus, VarName (LowercaseIdent "b")
+      ))
+    );
+
+  make_ast_converter_test
+    "semicolon expression"
+    "1;2"
+    (Sequential (Constant (Int 1), Constant (Int 2)));
+
+  make_ast_converter_test
+    "times infix expression"
+    "1 * 2"
+    (InfixOp (Constant (Int 1), Times, (Constant (Int 2))));
+
+  make_ast_converter_test
+    "divide infix expression"
+    "1 / 2"
+    (InfixOp (Constant (Int 1), Divide, (Constant (Int 2))));
+
+  make_ast_converter_test
+    "negation expression"
+    "~-1"
+    (PrefixOp (Negation, Constant (Int 1)));
 ]
 
 let suite = "test suite"  >::: List.flatten [
   tokenizer_tests;
-  parser_tests;
-  renderer_tests;
+  ast_converter_tests;
 ]
 
 let _ = run_test_tt_main suite
