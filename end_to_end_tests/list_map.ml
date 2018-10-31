c7a3ccb08ce3cf26f6ee7a6c0f3893f6351c87af
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-10-31 16:37:20 -0400

    Added more e2e tests

--- end_to_end_tests/list_map.ml
+++ end_to_end_tests/list_map.ml
@@ -2,6 +2,6 @@ let temp = ['a'; 'b'; '\n'; '\t'; 'l'; '\n']
 
 let rec print_chars tr = match tr with
   | h::t -> print_string (String.make 1 (Char.uppercase_ascii h));print_chars t
-  | [] -> 0;;
+  | [] -> ();;
 
 print_chars temp;;
---
+++ end_to_end_tests/pattern_matching_function_unit.ml
@@ -0,0 +1,3 @@
+let lol () = 1123.5;;
+
+print_float (lol ())
