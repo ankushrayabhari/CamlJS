32c340af996a0c11011c29b99d14373e21485ced
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-10-31 15:36:42 -0400

    Updated e2e tests to have calls to modules
    - String, Char

--- end_to_end_tests/anon_mult.ml
+++ end_to_end_tests/anon_mult.ml
@@ -1,2 +1,3 @@
 let result = (fun x y -> x * y) 100 3;;
+
 Pervasives.print_endline (string_of_int result)
--- end_to_end_tests/list_map.ml
+++ end_to_end_tests/list_map.ml
@@ -1,7 +1,7 @@
 let temp = ['a'; 'b'; '\n'; '\t'; 'l'; '\n']
 
 let rec print_chars tr = match tr with
-  | h::t -> print_char h;print_chars t
+  | h::t -> print_string (String.make 1 (Char.uppercase_ascii h));print_chars t
   | [] -> 0;;
 
 print_chars temp;;
