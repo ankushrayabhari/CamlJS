6c62a2a8fffaccdc00408ab24a524c816e23296d
Peter Husisian <pgh36@cornell.edu>
2018-10-31 20:30:36 -0400

    documented file_helper.mli

--- file_helper.mli
+++ file_helper.mli
@@ -1,3 +1,12 @@
+(**
+   - [read_file rel_path] is the string contained in the file
+   - found at the relative file path, [rel_path]
+ **)
 val read_file : string -> string
 
+(**
+   - [write_file rel_path to_write] writes [to_write]
+   - into a file stored at [rel_path], creating it if
+   - it does not exist.
+**)
 val write_file : string -> string -> unit
