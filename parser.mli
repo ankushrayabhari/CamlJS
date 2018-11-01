ed1da901dd6c912e11c1b23a6517d362726d617a
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-11-01 17:00:13 -0400

    Added Parser title

--- parser.mli
+++ parser.mli
@@ -1,4 +1,6 @@
-(** 
+(** Converts from a Token array to a Parse Tree *)
+
+(**
  * A [parse_tree] is a representation of the program as a tree of its
  * components, where the leaves in the tree read from left to right are the
  * tokens of the program in order from left to right:
@@ -6,14 +8,14 @@
  * - [Node lst] is a non-leaf node in the tree where [lst] is a
  * [parse_tree list] of its children nodes.
  *
- * Note that all unit production paths in the parse tree get squashed to a 
+ * Note that all unit production paths in the parse tree get squashed to a
  * single edge.
  *)
 type parse_tree =
   | Token of Token.t
   | Node of parse_tree list
 
-(** 
+(**
  * A [parse arr] is a [parse_tree tr] representing the token array [arr].
  *)
 val parse : Token.t array -> parse_tree
