bc4893b006baaab53e1c2b94aba954b77f50e915
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-11-01 17:03:29 -0400

    Added title for renderer

--- renderer.mli
+++ renderer.mli
@@ -1,5 +1,7 @@
+(** Converts the AST to JS Code *)
+
 (**
-* [render tr] is the JavaScript code equivalent to the code represented by 
+* [render tr] is the JavaScript code equivalent to the code represented by
 * abstract syntax tree [tr].
 *)
 val render : Ast.t -> string
