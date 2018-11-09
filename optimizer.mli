7e716c94d331f64753a08b452eb536a4b9900844
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-11-08 20:38:23 -0500

    Moved documentation for optimizer into right file

--- optimizer.mli
+++ optimizer.mli
@@ -3,5 +3,12 @@
 (**
  * [optimize ast] is another Abstract syntax tree equivalent to [ast] but that
  * is more optimal in the final rendered JavaScript.
+ *
+ * The following optimizations are run:
+ * - Deduplication of identifiers: {b See:} [Deduplicate_ident]
+ * - Removal of unused bindings: {b See:} [Unused_binding_optimizer]
+ * - Function call squashing: {b See:} [Call_squasher]
+ * - Uncurrying of functions: {b See:} [Curry_optimizer]
+ * - Identify tail recursive functions: {b See:} [Tail_rec_optimizer]
  *)
 val optimize : Ast.t -> Ast.t
