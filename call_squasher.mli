3b9807ce952df260950b992af07b4a606f3fdca5
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2018-11-08 20:36:39 -0500

    Updated documentation for call squasher and ident deduplicator

--- call_squasher.mli
+++ call_squasher.mli
@@ -6,14 +6,27 @@
  *
  * {b Example:}
  * - From the immediate AST conversion on the parse tree a function call [f 1 2]
- * will be transformed into: {ul
- * {li [FunctionCall (FunctionCall (VarName "f", [Constant (Int 1)], true),
-       [Constant (Int 2)], true)]}
- * }
+ * will be transformed into:
+ * {v
+ *    FunctionCall (
+ *      FunctionCall (
+ *        VarName "f",
+ *        [Constant (Int 1)],
+ *        true
+ *      ),
+ *      [Constant (Int 2)],
+ *      true
+ *    )
+ * v}
  * - However, because the function [f] is semantically applied to both [1] and
- * [2], this will be transformed into: {ul
- * {li [FunctionCall (VarName "f", [Constant (Int 1); Constant (Int 2)], true)]}
- * }
+ * [2], this will be transformed into:
+ * {v
+ *    FunctionCall (
+ *      VarName "f",
+ *      [Constant (Int 1); Constant (Int 2)],
+ *      true
+ *    )
+ * v}
  *
  * Any non-function call nodes are ignored and returned as is.
  *)
--- deduplicate_ident.mli
+++ deduplicate_ident.mli
@@ -1,1 +1,52 @@
+(** Deduplicates any identifiers. *)
+
+(**
+ * [optimize ast] transforms [ast] into an equivalent one with any lowercase
+ * ident deduplicated and assigned to a unique name.
+ *
+ * Record properties, variant names, module names and functions in built in
+ * modules are not renamed.
+ *
+ * {b Example:}
+ * - From the immediate AST conversion on the parse tree a function call
+ * [let x = 1 in let x = 2 in x] will be transformed into:
+ * {v
+ *    LetBinding (
+ *      VarAssignment (
+ *        ValueNamePattern "x",
+ *        true,
+ *        Constant (Int 1)
+ *      ),
+ *      LetBinding (
+ *        VarAssignment (
+ *          ValueNamePattern "x",
+ *          true,
+ *          Constant (Int 2)
+ *        ),
+ *        VarName "x"
+ *      )
+ *    )
+ * v}
+ * - The transformed ast will deduplicate [x] and assign each binding a
+ * unique identifier:
+ * {v
+ *    LetBinding (
+ *      VarAssignment (
+ *        ValueNamePattern "a",
+ *        true,
+ *        Constant (Int 1)
+ *      ),
+ *      LetBinding (
+ *        VarAssignment (
+ *          ValueNamePattern "b",
+ *          true,
+ *          Constant (Int 2)
+ *        ),
+ *        VarName "b"
+ *      )
+ *    )
+ * v}
+ *
+ * Aside from renaming identifiers, no modifications are made to the tree.
+ *)
 val optimize : Ast.t -> Ast.t
